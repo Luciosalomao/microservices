e044d9f94fc74a6f6c8f9b4db95a7ee216a5044e
lucio <luciomss@gmail.com>
2022-11-02 21:52:26 -0300

    Correção classe pessoa
    Correção classe pessoa

--- SlnWebApi/WebApiRest/WebApiRest/Model/Pessoa.cs
+++ SlnWebApi/WebApiRest/WebApiRest/Model/Pessoa.cs
@@ -6,7 +6,6 @@
         public string PrimeiroNome { get; set; }
         public string Sobrenome { get; set; }
         public string Endereco { get; set; }
-        public string MyProperty { get; set; }
         public string Genero { get; set; }
     }
 }
